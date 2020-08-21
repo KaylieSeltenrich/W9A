abadfd35653f41a1129bfb59d99a6aab5186228b
orcinusorca1995 <kaylie.seltenrich@gmail.com>
2020-08-20 20:44:17 -0600

    Finished Assignment W9B and attempted the bonus

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
-var userAge = 12;
-var is_subscribed = true;
+var userAges = [11,12,13,14,15,16,17,18,19,20];
+var subscriptionStatus = [true,false,true,true,false,true,false,false,true,true];
+var array = userAges && subscriptionStatus;
 
-if(userAge < 18 && is_subscribed) {
-    console.log("User is younger than 18 and is subscribed");
-   }  else if (userAge < 18 && !is_subscribed) {
-    console.log("User is younger than 18 and is not subscribed");
-   }
-   else if (userAge >=18 && !is_subscribed) {
-       console.log("User is older than 18 and is not subscribed");       
-   } else if (userAge >= 18 && is_subscribed) {
-    console.log("User is older than 18 and is subscribed");
-   }
 
+for (var counter = 0; counter < array.length; counter++)
+{ 
+
+    if(userAges[counter] < 18 && subscriptionStatus[counter]) {
+        console.log("User is younger than 18 and is subscribed");
+       }  else if (userAges[counter] < 18 && !subscriptionStatus[counter]) {
+        console.log("User is younger than 18 and is not subscribed");
+       }
+       else if (userAges[counter] >=18 && !subscriptionStatus[counter]) {
+           console.log("User is older than 18 and is not subscribed");       
+       } else if (userAges[counter] >=18 && subscriptionStatus[counter]) {
+        console.log("User is older than 18 and is subscribed");
+       } else {console.log("Error")}
+    
+}
